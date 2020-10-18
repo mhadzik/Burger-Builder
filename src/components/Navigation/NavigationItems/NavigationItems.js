a9a5d614b1634bc93bf8990069e5dc1c1074948f
Mikolaj Hadzik <mikolaj.hadzik@gmail.com>
2020-10-18 16:17:28 +0200

    Add navigation

---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,18 @@
+
+
+import React from 'react'
+import './NavigationItems.scss'
+import NavigationItem from './NavigationItem/NavigationItem'
+
+const navigationItems = (props) => (
+    <ul className="NavigationItems">
+        <NavigationItem link="/" active>
+            Burger Builder
+        </NavigationItem>
+        <NavigationItem link="/">
+            Checkout
+        </NavigationItem>
+    </ul>
+
+)
+export default navigationItems;
